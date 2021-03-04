ebe9f3e874260cf444a7026f009481f35d5c2a0c
Tin Vujasinović <tin.vujasinovic@info-novitas.com>
2021-03-04 12:54:04 +0100

    Added reporting support

---
+++ stomatoloska-ordinacija/Reports/Forms/OperationsByDate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace stomatoloska_ordinacija.Reports.Forms
+{
+    public partial class OperationsByDate : Form
+    {
+        public OperationsByDate()
+        {
+            InitializeComponent();
+        }
+
+        private void ReportViewerForm1_Load(object sender, EventArgs e)
+        {
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
